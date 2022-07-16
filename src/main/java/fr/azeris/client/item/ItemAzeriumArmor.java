a8b4f1e1f2e155b26e7703ff587b4e8f22c4eb7b
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2022-07-16 23:35:55 +0200

    sa

--- src/main/java/fr/azeris/client/item/ItemAzeriumArmor.java
+++ src/main/java/fr/azeris/client/item/ItemAzeriumArmor.java
@@ -1,21 +1,39 @@
 
 package fr.azeris.client.item;
 
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import fr.azeris.client.procedure.ProcedureCasquep;
+import fr.azeris.client.creativetab.TabAzerisTab;
+import fr.azeris.client.ElementsAzerisMod;
+
 @ElementsAzerisMod.ModElement.Tag
 public class ItemAzeriumArmor extends ElementsAzerisMod.ModElement {
-
 	@GameRegistry.ObjectHolder("azeris:azerium_armorhelmet")
 	public static final Item helmet = null;
-
 	@GameRegistry.ObjectHolder("azeris:azerium_armorbody")
 	public static final Item body = null;
-
 	@GameRegistry.ObjectHolder("azeris:azerium_armorlegs")
 	public static final Item legs = null;
-
 	@GameRegistry.ObjectHolder("azeris:azerium_armorboots")
 	public static final Item boots = null;
-
 	public ItemAzeriumArmor(ElementsAzerisMod instance) {
 		super(instance, 18);
 	}
@@ -24,9 +42,7 @@ public class ItemAzeriumArmor extends ElementsAzerisMod.ModElement {
 	public void initElements() {
 		ItemArmor.ArmorMaterial enuma = EnumHelper.addArmorMaterial("AZERIUM_ARMOR", "azeris:test", 20, new int[]{10, 20, 25, 10}, 10,
 				(net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("")), 3f);
-
 		elements.items.add(() -> new ItemArmor(enuma, 0, EntityEquipmentSlot.HEAD) {
-
 			@Override
 			public void onArmorTick(World world, EntityPlayer entity, ItemStack itemstack) {
 				super.onArmorTick(world, entity, itemstack);
@@ -35,20 +51,15 @@ public class ItemAzeriumArmor extends ElementsAzerisMod.ModElement {
 				int z = (int) entity.posZ;
 				{
 					Map<String, Object> $_dependencies = new HashMap<>();
-
 					$_dependencies.put("entity", entity);
-
 					ProcedureCasquep.executeProcedure($_dependencies);
 				}
 			}
 		}.setUnlocalizedName("azerium_armorhelmet").setRegistryName("azerium_armorhelmet").setCreativeTab(TabAzerisTab.tab));
-
 		elements.items.add(() -> new ItemArmor(enuma, 0, EntityEquipmentSlot.CHEST).setUnlocalizedName("azerium_armorbody")
 				.setRegistryName("azerium_armorbody").setCreativeTab(TabAzerisTab.tab));
-
 		elements.items.add(() -> new ItemArmor(enuma, 0, EntityEquipmentSlot.LEGS).setUnlocalizedName("azerium_armorlegs")
 				.setRegistryName("azerium_armorlegs").setCreativeTab(TabAzerisTab.tab));
-
 		elements.items.add(() -> new ItemArmor(enuma, 0, EntityEquipmentSlot.FEET).setUnlocalizedName("azerium_armorboots")
 				.setRegistryName("azerium_armorboots").setCreativeTab(TabAzerisTab.tab));
 	}
@@ -61,5 +72,4 @@ public class ItemAzeriumArmor extends ElementsAzerisMod.ModElement {
 		ModelLoader.setCustomModelResourceLocation(legs, 0, new ModelResourceLocation("azeris:azerium_armorlegs", "inventory"));
 		ModelLoader.setCustomModelResourceLocation(boots, 0, new ModelResourceLocation("azeris:azerium_armorboots", "inventory"));
 	}
-
 }
--- src/main/java/fr/azeris/client/procedure/ProcedureCasquep.java
+++ src/main/java/fr/azeris/client/procedure/ProcedureCasquep.java
@@ -1,8 +1,16 @@
 package fr.azeris.client.procedure;
 
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.init.MobEffects;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import fr.azeris.client.ElementsAzerisMod;
+
 @ElementsAzerisMod.ModElement.Tag
 public class ProcedureCasquep extends ElementsAzerisMod.ModElement {
-
 	public ProcedureCasquep(ElementsAzerisMod instance) {
 		super(instance, 24);
 	}
@@ -12,12 +20,8 @@ public class ProcedureCasquep extends ElementsAzerisMod.ModElement {
 			System.err.println("Failed to load dependency entity for procedure Casquep!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof EntityLivingBase)
 			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, (int) 1, (int) 2, (false), (false)));
-
 	}
-
 }
