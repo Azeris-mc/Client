e535082dde642caa6ff749fc14684e15caa979b8
RGreenWolf <57684328+RGreenWolf@users.noreply.github.com>
2022-07-16 23:17:21 +0200

    sa

---
+++ src/main/java/fr/azeris/client/procedure/ProcedureStartgame.java
@@ -0,0 +1,15 @@
+package fr.azeris.client.procedure;
+
+import java.util.Map;
+
+import fr.azeris.client.ElementsAzerisMod;
+
+@ElementsAzerisMod.ModElement.Tag
+public class ProcedureStartgame extends ElementsAzerisMod.ModElement {
+	public ProcedureStartgame(ElementsAzerisMod instance) {
+		super(instance, 1);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+	}
+}
